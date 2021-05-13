8c090bcdc66ff32a7e0065a544e7bf6ea2dd488e
Chris Hartley <chartley.it@gmail.com>
2021-05-12 22:27:34 -0400

    well fuck me

--- adventofcode/src/2018/day1/bincalib.go
+++ adventofcode/src/2018/day1/bincalib.go
@@ -40,7 +40,7 @@ func (o *Offset) insert(k int) error {
 			err = o.left.insert(k)
 		}
 	} else {
-		fmt.Println("Found duplicate!")
+		// fmt.Println("Found duplicate!")
 		err = errors.New("Duplicate Found")
 	}
 	return err
@@ -60,17 +60,17 @@ func search(input []int) int {
 }
 
 // calibrate runs list of frequency adjustments and returns frequency
-func (b *Bike) calibrate(c []int) {
+func (b *Bike) calibrate(c []int) (int, error) {
 	// fmt.Println("Calibrations to execute", len(c))
 	for _, adj := range c {
 		// b.hist = append(b.hist, b.freq)
 		b.freq = b.freq + adj
 		err := b.offset.insert(b.freq)
 		if err != nil {
-			fmt.Println("Duplicate! ", b.freq)
-			return
+			return b.freq, err
 		}
 	}
+	return 0, nil
 }
 
 func main() {
@@ -81,18 +81,21 @@ func main() {
 	bike := &Bike{freq: 0, offset: &Offset{key: 0}}
 	fmt.Println("Biginning bike calibration")
 	fmt.Println("Calibrations to execute", len(calibrations))
-	retry := 100
+	retry := 300
+	attempt := 0
 	for i := 0; i < retry; i++ {
-		bike.calibrate(calibrations)
-		// fmt.Println("Initial frequency", bike.freq)
+		attempt++
+		fmt.Println("Initial frequency", bike.freq)
+		f, err := bike.calibrate(calibrations)
 		// fmt.Println("Frequency history", bike.hist)
 		// fmt.Println("Calibration offset", bike.offset)
-		// if bike.offset != 0 {
-		// 	fmt.Println("Calibration offset", bike.offset)
-		// 	fmt.Println("Bike calibration completed")
-		// 	break
-		// }
+		if err != nil {
+			fmt.Println("Calibration offset", f)
+			fmt.Println("Bike calibration completed")
+			break
+		}
+		fmt.Println("Ending frequency", bike.freq)
 		fmt.Println("Retrying calibration...")
 	}
-	fmt.Printf("\nGiving up after %d attempts\nAttempted %d calibrations", retry, count)
+	fmt.Printf("\nAttempted calibrations %d\n", attempt)
 }
